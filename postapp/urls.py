843a3acb303ad03baccd98845ca70117d82b51c9
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2020-06-08 20:59:34 +0600

    Second Task Completed. Mind the ugly UI though :p

--- postapp/urls.py
+++ postapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from . import views, admin
 
 urlpatterns = [
-    path('', views.indexView, name='index'),
+    path('',            views.index,        name='index'),
+    path('dashboard/',  views.dashboard,    name='dashboard'),
+    path('login/',      views.site_login,   name='login'),
+    path('signup/',     views.signup,       name='signup'),
+    path('logout/',     views.site_logout,  name='logout'),
 ]
--- postapp/views.py
+++ postapp/views.py
@@ -1,6 +1,52 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 
-def indexView(request):
-    return HttpResponse("Hello world !")
+def index(request):
+    return render(request, 'index.html')
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+@login_required
+def site_logout(request):
+    logout(request)
+    return redirect('index')
+
+
+def signup(request):
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        
+        return render(request, 'dashboard.html')
+        
+    else:
+        form = UserCreationForm()
+        context = {'form': form}
+
+        return render(request, 'signup.html', context)
+
+
+def site_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            print("username: ", username, " password: ", password)
+            return redirect('dashboard')
+
+        return render(request, 'login.html')
+    
+    else:
+        return render(request, 'login.html')
