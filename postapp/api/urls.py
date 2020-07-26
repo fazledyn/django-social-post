ecc9be993c1181dda828476163d430bd4346ce8a
Ataf Fazledin <1705066@ugrad.cse.buet.ac.bd>
2020-07-27 05:08:57 +0600

    PostViewSet, Router has been added

--- postapp/api/urls.py
+++ postapp/api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from postapp.api.views import PostViewSet
 
 app_name = "postapp"
 
+router = routers.SimpleRouter(trailing_slash=True)
+router.register(r'post', PostViewSet)
+
 urlpatterns = [
-    
+    router.urls
 ]
--- postapp/api/views.py
+++ postapp/api/views.py
@@ -1,8 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import status
-from rest_framework import viewsets
-
+from rest_framework import status, viewsets
 
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
@@ -10,29 +8,43 @@ from django.shortcuts import get_object_or_404
 
 from postapp.api.serializers import PostSerializer, UserSerializer
 from postapp.models import Post
-from postapp.models import Post
 
 """    Blog Post App
 """
 
-class PostViewSet(viewsets.ViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     
     def list(self, request):
         queryset = Post.objects.all()
         serializer = PostSerializer(queryset, many=True)
         return Response(serializer.data)
-        
+
 
     def create(self, request):
-        
-        pass
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    def retrieve(self, request):
+
+    def retrieve(self, request, pk=None):
         queryset = Post.objects.all()
+        post = get_object_or_404(queryset, pk=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
 
-    def update(self, request):
-        pass
 
-    def destroy(self, request):
-        pass
+    def update(self, request, pk=None):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+        
 
+    def destroy(self, request, pk=None):
+        queryset = Post.objects.all()
+        post = get_object_or_404(queryset, pk=pk)
+        post.delete()
+        return Response(status=status.HTTP_200_SUCCESS)
